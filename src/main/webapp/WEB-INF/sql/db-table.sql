85a69f445d05a144d7905d8da8b73d035047e2a6
starble25 <starble25@gmail.com>
2025-02-07 10:59:49 +0900

    DB table 추가

---
+++ src/main/webapp/WEB-INF/sql/db-table.sql
@@ -0,0 +1,38 @@
+-- 회원가입
+CREATE TABLE USERS (
+	id NUMBER PRIMARY KEY,
+	email VARCHAR2(64) NOT NULL UNIQUE,
+	pw VARCHAR2(64) NOT NULL,
+	name VARCHAR2(32),
+	tel VARCHAR2(32),
+	jumin VARCHAR2(32),
+	position_code VARCHAR2(32),
+	dept_code VARCHAR2(32),
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
+	cmp_id NUMBER
+);
+
+CREATE SEQUENCE USERS_PK
+START WITH 1
+INCREMENT BY 1
+NOCYCLE;
+
+/*
+	insert into users ( users_id, email, pw )
+	values ( USERS_PK.nextval, '123@aaa.com', '11' );
+*/
+
+-- 주소록
+CREATE TABLE ADDRESS (
+	id NUMBER PRIMARY KEY,
+    my_id NUMBER REFERENCES USERS(id) ON DELETE CASCADE,
+    add_id NUMBER REFERENCES USERS(id) ON DELETE CASCADE,
+	favorite VARCHAR(1)
+);
+
+CREATE SEQUENCE ADDRESS_PK
+START WITH 1
+INCREMENT BY 1
+NOCYCLE;
+
