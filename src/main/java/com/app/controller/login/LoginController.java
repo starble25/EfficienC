6a486ca659b617cfe41dc68ceec03573ce4d2a96
Han-kyu <kwakbab9@gmail.com>
2025-02-07 14:15:29 +0900

    merge 후 수정

--- src/main/java/com/app/controller/login/LoginController.java
+++ src/main/java/com/app/controller/login/LoginController.java
@@ -1,14 +1,20 @@
 package com.app.controller.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.app.dto.user.User;
+import com.app.service.UserService;
 
 @Controller
 public class LoginController {
 
 	// id 와 pw 가 동시에 일치하는 사용자 users Table find
 	// users Table -> cmp_id 체크 후 사이트 이동
+	@Autowired
+	UserService userService;
 
 	@GetMapping("/register")
 	public String registerUser() {
